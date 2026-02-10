ffba0911fcc2fd6456b85490e14f046e8470e655
muqeetmughal <muqeetmughal786@gmail.com>
2026-02-10 05:06:56 +0500

    improved deploy.sh

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -5,7 +5,7 @@ set -e  # Stop on first error
 APP_DIR=~/infintrix-erp/apps/infintrix_atlas
 BENCH_DIR=~/infintrix-erp
 SITE_NAME=$1  # pass the site name as argument
-
+NODE_VERSION=20
 echo "🚀 ==> Pulling latest code..."
 cd $APP_DIR
 git reset --hard
@@ -15,9 +15,25 @@ git pull
 echo "📦 ==> Installing Python dependencies..."
 cd $BENCH_DIR
 source env/bin/activate
+
+# --- Setup Node.js & Yarn ---
+echo "==> Ensuring Node.js & Yarn..."
+export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+if ! command -v node &> /dev/null; then
+    echo "Node not found. Installing Node $NODE_VERSION..."
+    nvm install "$NODE_VERSION"
+fi
+nvm use "$NODE_VERSION"
+export PATH="$HOME/.nvm/versions/node/v$NODE_VERSION/bin:$PATH"
+
 
 bench setup requirements infintrix_atlas
+
+if ! command -v yarn &> /dev/null; then
+    echo "Yarn not found. Installing..."
+    npm install -g yarn
+fi
 
 echo "🏗️  ==> Building assets..."
 bench build --apps infintrix_atlas --skip-redis
