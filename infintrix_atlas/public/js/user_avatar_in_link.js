cf488ccfab871402246c188425bcf6950527b443
muqeetmughal <muqeetmughal786@gmail.com>
2026-08-06 00:02:48 +0500

    feat: show avatar on selected User link value
    Render the user's avatar inside the link field whenever a value is set,
    so it is visible immediately on form load (not only after focusing).
    Render inside .link-field for correct positioning, hook refresh_input in
    addition to the value setters, and cache fetched user images.

--- infintrix_atlas/public/js/user_avatar_in_link.js
+++ infintrix_atlas/public/js/user_avatar_in_link.js
@@ -69,3 +69,68 @@ frappe.ui.form.ControlLink.prototype.fetch_user_avatars_for_open_dropdown = asyn
 			frame.textContent = "";
 		});
 };
+
+const __atlas_original_set_input_value = frappe.ui.form.ControlLink.prototype.set_input_value;
+frappe.ui.form.ControlLink.prototype.set_input_value = function (value) {
+	__atlas_original_set_input_value.call(this, value);
+	this.render_selected_user_avatar();
+};
+
+const __atlas_original_set_formatted_input = frappe.ui.form.ControlLink.prototype.set_formatted_input;
+frappe.ui.form.ControlLink.prototype.set_formatted_input = function (value) {
+	__atlas_original_set_formatted_input.call(this, value);
+	this.render_selected_user_avatar();
+};
+
+const __atlas_original_refresh_input = frappe.ui.form.ControlLink.prototype.refresh_input;
+frappe.ui.form.ControlLink.prototype.refresh_input = function () {
+	__atlas_original_refresh_input.call(this);
+	this.render_selected_user_avatar();
+};
+
+frappe.atlas_user_images = frappe.atlas_user_images || {};
+
+frappe.ui.form.ControlLink.prototype.render_selected_user_avatar = function () {
+	const me = this;
+	if (me.get_options() !== "User" || !me.$input_area) return;
+
+	const $field = me.$input_area.find(".link-field");
+	$field.find(".atlas-selected-avatar").remove();
+
+	const name = me.get_input_value() || me.value;
+	if (!name || String(name).includes("__link_option")) {
+		me.$input && me.$input.css("padding-left", "");
+		return;
+	}
+
+	const $avatar = $(
+		'<span class="atlas-selected-avatar" style="position:absolute;left:6px;top:50%;transform:translateY(-50%);z-index:1;pointer-events:none;"></span>'
+	).prependTo($field);
+	$avatar.html(frappe.avatar(name, "avatar-small"));
+
+	const cached = frappe.atlas_user_images[name];
+	if (cached) {
+		set_atlas_avatar_image($avatar, cached);
+	} else {
+		me.fetch_user_avatar_image(name).then((url) => {
+			if (!url) return;
+			frappe.atlas_user_images[name] = url;
+			set_atlas_avatar_image($avatar, url);
+		});
+	}
+
+	me.$input && me.$input.css("padding-left", "40px");
+};
+
+function set_atlas_avatar_image($avatar, url) {
+	const frame = $avatar.find(".avatar-frame").get(0);
+	if (!frame) return;
+	frame.style.backgroundImage = `url("${url}")`;
+	frame.classList.remove("standard-image");
+	frame.textContent = "";
+}
+
+frappe.ui.form.ControlLink.prototype.fetch_user_avatar_image = async function (name) {
+	const res = await frappe.db.get_value("User", name, "user_image");
+	return res && res.message && res.message.user_image;
+};
